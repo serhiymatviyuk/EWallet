a22d6431bfcffd01b8d66255e74cb5d356438787
Serhii Matviiuk <serhij.matvijuk@gmail.com>
2022-09-30 14:19:49 +0300

    Updated login form

--- ewallet.client/src/components/LoginForm/LoginForm.tsx
+++ ewallet.client/src/components/LoginForm/LoginForm.tsx
@@ -1,45 +1,59 @@
+import { Button } from "antd";
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
 import { authenticationActions } from "../../reducers";
 import "./LoginForm.css";
 
 const LoginForm = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const [email, setEmail] = useState<string>('admin@mail.com');
-  const [password, setPassword] = useState<string>('admiN@');
+    const [email, setEmail] = useState<string>('admin@mail.com');
+    const [password, setPassword] = useState<string>('admiN@');
 
-  const submitLogin = async (event: any) => {
-    event.preventDefault();
+    const submitLogin = async (event: any) => {
+        event.preventDefault();
+        console.log('test');
 
-    dispatch(authenticationActions.api_requestToken({ username: email, password: password }));
-  }
+        dispatch(authenticationActions.api_requestToken({ username: email, password: password }));
+    }
 
-  return (
-    <div className="login-block">
-      <form onSubmit={submitLogin}>
-        <div className="mb-3">
-          <h1 className="display-5">Login</h1>
-        </div>
-        <div className="mb-3">
-          <label>Email</label>
-          <input type="email" value={email} className="form-control" onChange={(e) => setEmail(e.target.value)} />
-        </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input type="password" value={password} className="form-control" onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <div className="d-grid gap-2">
-          <input type="submit" className="btn btn-primary" value="Login" />
-        </div>
-        <div className="mb-3">
-          <a href="/recover" className="link-primary">
-            Forgot password?
-          </a>
-        </div>
-      </form>
-    </div>
-  );
+    return (
+        <>
+            <div className="component__SignIn_VIEW">
+                <div className="view__WRAPPER">
+
+                    <div className="view__CONTENT">
+                        <h2>Log in to your account</h2>
+
+                        <form onSubmit={submitLogin}>
+                            <div className="input__CONTROL full_w">
+                                <label className="input__TITLE">Email</label>
+                                <input type="email" value={email} className="ant-input ant-input-lg" onChange={(e) => setEmail(e.target.value)} />
+                            </div>
+
+                            <div className="input__CONTROL full_w">
+                                <label className="input__TITLE">Password</label>
+                                <input type="password" value={password} className="ant-input ant-input-lg" onChange={(e) => setPassword(e.target.value)} />
+                            </div>
+
+                            <div className="button__CONTAINER">
+                                <Button size={"large"} type="primary" htmlType="submit" className="signin__button">
+                                    Sign In
+                                </Button>
+                            </div>
+                        </form>
+
+                        <div className="contactWrapper">
+                            <a href="/recover" className="link-primary">
+                                <p> Forgot password?</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default LoginForm;
